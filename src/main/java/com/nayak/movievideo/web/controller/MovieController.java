d98665367ae954c51092b9ba80fff93b5c201fc5
nayak <wjdduq914@gmail.com>
2018-06-06 17:58:10 +0900

    Add. 오늘 크롤링한 영화 리스트 모델에 추가

--- src/main/java/com/nayak/movievideo/web/controller/MovieController.java
+++ src/main/java/com/nayak/movievideo/web/controller/MovieController.java
@@ -1,8 +1,14 @@
 package com.nayak.movievideo.web.controller;
 
+import com.nayak.movievideo.domain.MovieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * Created by nayak on 2018. 6. 6..
@@ -11,8 +17,14 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @RequestMapping("/movie")
 public class MovieController {
 
+    private static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd");
+
+    @Autowired
+    private MovieRepository movieRepository;
+
     @RequestMapping(path = "/list", method = RequestMethod.GET)
-    public String moveList() {
+    public String moveList(Model model) {
+        model.addAttribute("movieList", movieRepository.findMovieByCrawlingDate(DATE_FORMAT.format(new Date())));
         return "movie/movie_list";
     }
 }
