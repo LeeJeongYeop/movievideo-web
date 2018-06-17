2815ab6e08bb2eee82fc812fefd4c0dbc04065a1
nayak <wjdduq914@gmail.com>
2018-06-17 21:38:53 +0900

    Add. 영상 상세화면 레이아웃

--- src/main/java/com/nayak/movievideo/web/controller/VideoController.java
+++ src/main/java/com/nayak/movievideo/web/controller/VideoController.java
@@ -1,5 +1,6 @@
 package com.nayak.movievideo.web.controller;
 
+import com.nayak.movievideo.domain.Video;
 import com.nayak.movievideo.domain.VideoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -7,6 +8,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.stream.Collectors;
 
 /**
  * Created by nayak on 2018. 6. 9..
@@ -22,5 +25,15 @@ public class VideoController {
     public String videoList(@RequestParam String movieTitle, Model model) {
         model.addAttribute("videoList", videoRepository.findVideoByMovieTitle(movieTitle));
         return "video/video_list";
+    }
+
+    @RequestMapping(path = "/detail", method = RequestMethod.GET)
+    public String videoDetail(@RequestParam String videoId, Model model) {
+        Video video = videoRepository.findVideoByVideoId(videoId);
+        model.addAttribute("video", video);
+        model.addAttribute("relatedVideoList", videoRepository.findVideoByMovieTitle(video.getMovieTitle()).stream()
+                .filter(relatedVideo -> !relatedVideo.getVideoId().equals(video.getVideoId()))
+                .collect(Collectors.toList()));
+        return "video/video_detail";
     }
 }
