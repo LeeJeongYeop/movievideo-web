fb50c21cc4ad4ac7ac90c486103a26569432753e
nayak <wjdduq914@gmail.com>
2018-06-23 18:34:45 +0900

    Update. js 주석 제거

--- src/main/resources/static/javascript/common.js
+++ src/main/resources/static/javascript/common.js
@@ -6,10 +6,8 @@ $(document).ready(function () {
     });
 
     $(".video-list-card").hover(function() {
-        // $(this).children(".card-body").children(".card-img-top").css("box-shadow", "0 19px 38px rgba(0,0,0,0.30), 0 15px 12px rgba(0,0,0,0.22)")
         $(this).children(".card-body").children().css("text-decoration", "underline")
     }, function() {
-        // $(this).children(".card-body").children(".card-img-top").css("box-shadow", "none");
         $(this).children(".card-body").children().css("text-decoration", "none")
     });
 
